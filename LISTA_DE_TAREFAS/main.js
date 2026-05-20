98e4abac51d278a7d227c75615b242db90600ca6
GABRIEL CARLOS PEREIRA <gabriel.pereira25@univille.br>
2026-05-20 10:50:09 -0300

    FEAT: termina Css

--- LISTA_DE_TAREFAS/main.js
+++ LISTA_DE_TAREFAS/main.js
@@ -16,23 +16,40 @@ const mostrarTarefas = () => {
             <span class="material-symbols-outlined delete">delete</span>
         </li>
       `;
-    });
+ });
+tarefa();
 };
 
 const tarefa = () => {
     document.querySelectorAll("li").forEach(e => {
         e.addEventListener("click" , a => {
             if(a.target.classList.value.includes("delete")){
-                
+                tarefas = tarefas.filter(b => b.id !== +e.id)
+                save();
+                mostrarTarefas();
             }
-        })
-    })
+
+            if(a.target.classList.value.includes("check")){
+                const index = tarefas.findIdex((i) => i.id === +e.id);
+                tarefas[index].check = !tarefas[index].check;
+                save()
+            }
+        });
+    });
+};
+
+const save = () => {
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
 }
 
 form.addEventListener("submit", (e) => {
    e.preventDefault();
    if(e.target[0].value.length > 0){
        tarefas.push({nome:e.target[0].value, id: Math.random(), check: false });
+       save();
        mostrarTarefas();
+       e.target.reset();
    }
 });
+
+window.addEventListener("load" , mostrarTarefas)
